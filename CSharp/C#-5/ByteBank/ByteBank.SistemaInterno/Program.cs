b5ce07c99ccbb7ddf3f87659f0c77f19ef22e412
Arthur Dantas <52866247+ArthurGBD@users.noreply.github.com>
2021-11-24 00:44:28 -0300

    Avançando em C# 5

---
+++ CSharp/C#-5/ByteBank/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 455789);
+        }
+    }
+}
