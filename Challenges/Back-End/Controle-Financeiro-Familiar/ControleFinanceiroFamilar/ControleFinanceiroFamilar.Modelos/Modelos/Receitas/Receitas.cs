4050908e8592bb30f71c228835b55069af67563a
Arthur Dantas <arte.arthur@hotmail.com>
2022-01-26 17:48:33 -0300

    Update

--- Challenges/Back-End/Controle-Financeiro-Familiar/ControleFinanceiroFamilar/ControleFinanceiroFamilar.Modelos/Modelos/Receitas/Receitas.cs
+++ Challenges/Back-End/Controle-Financeiro-Familiar/ControleFinanceiroFamilar/ControleFinanceiroFamilar.Modelos/Modelos/Receitas/Receitas.cs
@@ -1,13 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ControleFinanceiroFamilar.Modelos.Modelos.Receitas
 {
     public class Receitas
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Informe a descrição de receita!")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Informe o valor da receita!")]
         public double Valor { get; set; }
 
+        [Required(ErrorMessage = "Informe a data desta receita!")]
         [Column(TypeName = "dateTime2")]
         public DateTime Data { get; set; }
     }
