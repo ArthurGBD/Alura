2373fc26589bca7534e89f602809f4bdcd022f7e
Arthur Dantas <52866247+ArthurGBD@users.noreply.github.com>
2022-02-09 23:53:06 -0300

    Finalizando parte 1

---
+++ CSharp/C#-PARALELISMO NO MUNDO REAL/teste/teste/Program.cs
@@ -0,0 +1,36 @@
+﻿
+static void Main(string[] args)
+{
+
+    //static long FatorialEmParalelo(int valor)
+    //{
+    //    long resultado = 0;
+
+    //    var t_calculo = new Thread(() => resultado = Fatorial(valor));
+    //    t_calculo.Start();
+
+    //    return resultado;
+    //}
+
+    // static long Fatorial(int valor)
+    //{
+    //    var resultado = 1L;
+    //    for (int i = valor; i >= 2; i--)
+    //        resultado *= i;
+    //    Console.WriteLine(resultado);
+    //    return resultado;
+    //    Console.WriteLine(" ");
+    //    Console.WriteLine(resultado);
+    //}
+
+    for (int i = 0; i < 10; i++)
+    {
+
+        var thread = new Thread(() => Thread.Sleep(5000));
+        var isAlive = thread.IsAlive;
+        Console.WriteLine(isAlive);
+    }
+
+}
+
+
