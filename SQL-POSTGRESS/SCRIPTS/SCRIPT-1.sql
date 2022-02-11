500498e9dfc8cc49ea91b82c30d730d2ed938c2a
Arthur Dantas <52866247+ArthurGBD@users.noreply.github.com>
2022-02-11 01:01:43 -0300

    sql

--- SQL-POSTGRESS/SCRIPTS/SCRIPT-1.sql
+++ SQL-POSTGRESS/SCRIPTS/SCRIPT-1.sql
@@ -13,3 +13,59 @@ CREATE TABLE aluno(
 );
 
 SELECT * FROM aluno;
+
+INSERT INTO aluno (
+	nome,
+	cpf,
+	observacao,
+	idade,
+	dinheiro,
+	altura,
+	ativo,
+	data_nascimento,
+	hora_aula,
+	matriculado_em
+				 )
+VALUES (
+	'Arthur',
+	'47579795841',
+	'Eai!',
+	22,
+	100.50,
+	1.89,
+	TRUE,
+	'1999-09-28',
+	'17:30:00',
+	'2022-02-10 10:45:50'
+);
+
+SELECT *
+	FROM aluno
+WHERE id = 1;
+
+UPDATE aluno
+	SET nome = 'Teste',
+	cpf = '12345678910',
+	observacao = 'Teste',
+	idade = 23,
+	dinheiro = 100.00,
+	altura = 1.90,
+	ativo = FALSE,
+	data_nascimento = '1975-01-15',
+	hora_aula = '13:00:00',
+	matriculado_em = '2020-01-02'
+	
+WHERE id = 1;
+
+SELECT *
+	FROM aluno
+	WHERE nome = 'Arthur';
+	
+DELETE
+	FROM aluno
+	WHERE nome = 'Teste';
+
+
+
+
+
