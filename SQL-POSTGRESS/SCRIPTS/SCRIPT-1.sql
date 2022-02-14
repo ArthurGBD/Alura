a6b73b46f94b50c4c6cffd6f82518721e950599c
Arthur Dantas <52866247+ArthurGBD@users.noreply.github.com>
2022-02-13 21:13:13 -0300

    Up

--- SQL-POSTGRESS/SCRIPTS/SCRIPT-1.sql
+++ SQL-POSTGRESS/SCRIPTS/SCRIPT-1.sql
@@ -150,4 +150,151 @@ WHERE nome LIKE 'Diogo'
 OR nome LIKE 'Rodrigo'
 OR nome LIKE 'Arthur';
  
+CREATE TABLE curso (
+	id INTEGER NOT NULL UNIQUE,
+	nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE curso (
+	id INTEGER PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL
+);
  
+INSERT INTO curso (id, nome) VALUES(NULL, NULL);
+INSERT INTO curso (id, nome) VALUES(1, NULL);
+INSERT INTO curso (id, nome) VALUES(NULL, 'HTML');
+
+INSERT INTO curso (id, nome) VALUES(1, 'HTML');
+INSERT INTO curso (id, nome) VALUES(2, 'JAVASCRIPT');
+
+SELECT * FROM curso;
+
+DROP TABLE aluno;
+ 
+CREATE TABLE aluno (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL
+);
+
+
+INSERT INTO aluno (nome) VALUES ('Diogo');
+INSERT INTO aluno (nome) VALUES ('Vínícius');
+
+SELECT * FROM aluno;
+
+DROP TABLE aluno_curso;
+
+CREATE TABLE aluno_curso (
+	aluno_id INTEGER,
+	curso_id INTEGER,
+	PRIMARY KEY (aluno_id, curso_id),
+	
+	FOREIGN KEY (aluno_id)
+		REFERENCES aluno (id),
+	
+	FOREIGN KEY (curso_id)
+		REFERENCES curso (id)
+);
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,3);
+
+
+
+SELECT * FROM aluno WHERE id = 1;
+SELECT * FROM curso WHERE id = 1;
+
+SELECT * FROM aluno WHERE id = 2;
+SELECT * FROM curso WHERE id = 1;
+
+SELECT * FROM aluno WHERE id = 3;
+SELECT * FROM curso WHERE id = 1;
+
+
+SELECT * FROM aluno;
+SELECT * FROM curso;
+
+SELECT aluno.nome AS aluno_nome,
+	   curso.nome AS curso_nome
+	FROM aluno
+	JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+	JOIN curso 	     ON curso.id 			 = aluno_curso.curso_id
+	
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES(2,2);
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES(2,2);
+
+INSERT INTO aluno (nome) VALUES('Arthur');
+
+INSERT INTO curso (id, nome) VALUES(3,'CSS');
+
+SELECT aluno.nome AS aluno_nome,
+	   curso.nome AS curso_nome
+	FROM aluno
+LEFT JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+LEFT JOIN curso 	  ON curso.id 	 	      = aluno_curso.curso_id
+
+SELECT aluno.nome AS aluno_nome,
+	   curso.nome AS curso_nome
+	FROM aluno
+RIGHT JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+RIGHT JOIN curso 	   ON curso.id 	 	       = aluno_curso.curso_id
+
+SELECT aluno.nome AS aluno_nome,
+	   curso.nome AS curso_nome
+	FROM aluno
+FULL JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+FULL JOIN curso 	  ON curso.id 	 	      = aluno_curso.curso_id
+
+SELECT aluno.nome AS aluno_nome,
+	   curso.nome AS curso_nome
+	FROM aluno
+CROSS JOIN curso
+
+INSERT INTO aluno (nome) VALUES('Jorge');
+
+DROP TABLE aluno_curso;
+
+CREATE TABLE aluno_curso (
+	aluno_id INTEGER,
+	curso_id INTEGER,
+	PRIMARY KEY (aluno_id, curso_id),
+	
+	FOREIGN KEY (aluno_id)
+		REFERENCES aluno (id)
+		ON DELETE CASCADE,
+	
+	FOREIGN KEY (curso_id)
+		REFERENCES curso (id)
+);
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (2,1);
+
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (3,1);
+INSERT INTO aluno_curso (aluno_id, curso_id) VALUES (1,3);
+
+SELECT * FROM aluno_curso;
+
+DELETE FROM aluno WHERE id = 1;
+
+SELECT aluno.nome AS aluno_nome,
+	   curso.nome AS curso_nome
+	FROM aluno
+	JOIN aluno_curso ON aluno_curso.aluno_id = aluno.id
+	JOIN curso 	     ON curso.id 			 = aluno_curso.curso_id
+	
+	SELECT * FROM aluno;
+	
+	UPDATE aluno
+		SET id = 10
+		WHERE id = 2;
+		
+	UPDATE aluno
+		SET id = 20
+		WHERE id = 4;
+
+
